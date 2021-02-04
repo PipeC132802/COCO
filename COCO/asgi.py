adcf7e97a927c6a8ab270f94e81698a7fd225e2f
Felipe Cerquera <andrescercal@hotmail.com>
2021-02-04 17:54:30 -0500

    channels config adjusted

--- COCO/asgi.py
+++ COCO/asgi.py
@@ -1,23 +1,22 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'COCO.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "COCO.settings")
+django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
 from Apps.ManageNotifications.routing import websocket_urlpatterns as notify_ws
 from Apps.ManageChats.routing import websocket_urlpatterns as chats_ws
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            [
-                notify_ws,
-                chats_ws
-            ]
+            notify_ws +
+            chats_ws
         )
     ),
 
