e636c21288ff8a4a7a045a6f7590b044990742d6
Felipe Cerquera <andrescercal@hotmail.com>
2021-02-10 19:16:57 -0500

    comments margin adjusted

--- COCO/settings.py
+++ COCO/settings.py
@@ -28,7 +28,7 @@ with open('/var/SECRET_KEY.txt') as f:
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Production settings
-DEBUG = True
+DEBUG = False
 """SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
